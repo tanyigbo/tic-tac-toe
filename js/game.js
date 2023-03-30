d5f3e38a1eb8b9d585e950b61d06af2770b9d7d5
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-03-30 12:46:32 -0500

    refactored game logic

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ class Game {
         this.winner = 9
         this.turnCount = 0
         this.winnerFound = false
-        this.continueGame = true
+        this.endGame = false
         this.cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
         this.downDiagonal = [9, 9, 9]
         this.upDiagonal = [9, 9, 9]
@@ -14,45 +14,56 @@ class Game {
         this.winner = 9
         this.turnCount = 0
         this.winnerFound = false
-        this.continueGame = true
+        this.endGame = false
         this.cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
         this.downDiagonal = [9, 9, 9]
         this.upDiagonal = [9, 9, 9]
     }
     fillSquare(row, column) {
-        if (cells[row][column] !== 9) {
-            cell[row][column] = this.activePlayer
+        if (this.cells[row][column] === 9) {
+            this.cells[row][column] = this.activePlayer
             if (row === column) {
-                this.downDiagonal[row] === this.activePlayer
+                this.downDiagonal[row] = this.activePlayer
             }
-            else if (row + column === 2) {
+            if (row + column == 2) {
                 this.upDiagonal[column] = this.activePlayer
             }
             this.turnCount += 1
-            this.winnerCheck()
+            this.winnerCheck(row, column)
             this.drawCheck()
             this.activePlayer = this.activePlayer === 0 ? 1 : 0
         }
     }
     winnerCheck(row, column) {
         let rowCheck = this.cells[row].reduce((sum, num) => { return sum + num })
-        let columnCheck = cells[0][column] + cells[1][column] + cells[2][column]
+        let columnCheck = this.cells[0][column] + this.cells[1][column] + this.cells[2][column]
         let downDiagCheck = this.downDiagonal.reduce((sum, num) => { return sum + num })
         let upDiagCheck = this.upDiagonal.reduce((sum, num) => { return sum + num })
 
+        for (let element of this.cells) {
+            console.log(element)
+        }
+
+        console.log(rowCheck, columnCheck, downDiagCheck, upDiagCheck)
         if (rowCheck === 0 || rowCheck === 3 || columnCheck === 0 || columnCheck === 3) {
+            console.log('Called Endgame')
             this.gameWon()
         }
         else if (downDiagCheck === 0 || downDiagCheck === 3 || upDiagCheck === 0 || upDiagCheck === 3) {
+            console.log('Called Endgame')
             this.gameWon()
         }
     }
     gameWon() {
         this.winner = this.activePlayer
         this.winnerFound = true
-        this.continueGame = false
+        this.endGame = true
+
+        console.log(`Game status Winner:${this.winner} Winner Found:${this.winnerFound} End Game:${this.endGame} Turn Count:${this.turnCount}`)
     }
     drawCheck() {
-        this.continueGame = this.turnCount === 9 ? false : true
+        if (!this.endGame) {
+            this.endGame = this.turnCount === 9 ? true : false
+        }
     }
 }
