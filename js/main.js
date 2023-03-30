42b019a3f938939af8c1a87b680033aa6fceffb2
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-03-29 22:30:05 -0500

    started DOM manipulation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-const boardGrid = document.querySelector('.board-grid')
-boardGrid.addEventListener('click', () => {
-    console.log('Test')
+const startBtn = document.querySelector('#start-btn')
+const xSymbol = document.querySelector('#x-sym')
+const oSymbol = document.querySelector('#y-sym')
+const winDisplay = document.querySelector('#win-banner')
+const board = document.querySelectorAll('.board-grid')
+
+startBtn.addEventListener('click', (event) => {
+
 })
+
+board.addEventListener('click', (event, e) => {
+
+})
+console.log(board)
