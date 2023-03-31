7d2c3a92ada3785137489b42cc68597b1a9b54f0
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-03-31 13:43:30 -0500

    more descriptive variable and method name

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ class Game {
         this.winner = 9
         this.turnCount = 0
         this.winnerFound = false
-        this.endGame = false
+        this.gameEnded = false
         this.cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
         this.downDiagonal = [9, 9, 9]
         this.upDiagonal = [9, 9, 9]
@@ -14,17 +14,22 @@ class Game {
         this.winner = 9
         this.turnCount = 0
         this.winnerFound = false
-        this.endGame = false
+        this.gameEnded = false
         this.cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
         this.downDiagonal = [9, 9, 9]
         this.upDiagonal = [9, 9, 9]
     }
+
     fillSquare(row, column) {
+        //Check if cell has already been filled (seperation of logic from implementation)
         if (this.cells[row][column] === 9) {
             this.cells[row][column] = this.activePlayer
+
+            //Checks if cell is in down-diagonal 
             if (row === column) {
                 this.downDiagonal[row] = this.activePlayer
             }
+            //Checks if cell is in up-diagonal 
             if (row + column == 2) {
                 this.upDiagonal[column] = this.activePlayer
             }
@@ -34,31 +39,33 @@ class Game {
             this.activePlayer = this.activePlayer === 0 ? 1 : 0
         }
     }
+
     winnerCheck(row, column) {
+        //Player 0 wins with sum of 0
+        //Player 1 wins with sum of 3
         let rowCheck = this.cells[row].reduce((sum, num) => { return sum + num })
         let columnCheck = this.cells[0][column] + this.cells[1][column] + this.cells[2][column]
         let downDiagCheck = this.downDiagonal.reduce((sum, num) => { return sum + num })
         let upDiagCheck = this.upDiagonal.reduce((sum, num) => { return sum + num })
 
         if (rowCheck === 0 || rowCheck === 3 || columnCheck === 0 || columnCheck === 3) {
-            console.log('Called Endgame')
-            this.gameWon()
+            this.playerWinsGame()
         }
         else if (downDiagCheck === 0 || downDiagCheck === 3 || upDiagCheck === 0 || upDiagCheck === 3) {
-            console.log('Called Endgame')
-            this.gameWon()
+            this.playerWinsGame()
         }
     }
-    gameWon() {
+
+    playerWinsGame() {
         this.winner = this.activePlayer
         this.winnerFound = true
-        this.endGame = true
-
-        console.log(`Game status Winner:${this.winner} Winner Found:${this.winnerFound} End Game:${this.endGame} Turn Count:${this.turnCount}`)
+        this.gameEnded = true
     }
+
     drawCheck() {
-        if (!this.endGame) {
-            this.endGame = this.turnCount === 9 ? true : false
+        //Ends game at 9 turns if not already ended by player win
+        if (!this.gameEnded) {
+            this.gameEnded = this.turnCount === 9 ? true : false
         }
     }
 }
