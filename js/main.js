e878b10bc9300efd1c5691c42e16994773ce8997
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-03-31 13:44:38 -0500

    removed duplicative code. added game status update

--- js/main.js
+++ js/main.js
@@ -3,113 +3,43 @@ function updateSquare(game, event) {
     const cell = event.target
     if (cell.dataset.status === 'E') {
         cell.setAttribute('data-status', 'F')
-        cell.innerText = `${game.activePlayer}`
+        cell.innerText = `${players[game.activePlayer]}`
         game.fillSquare(Number(cell.dataset.row), Number(cell.dataset.column))
         endGameCheck(game)
     }
 }
 
 function endGameCheck(activeGame) {
-    if (activeGame.endGame) {
-        console.log("Game Ended")
-        displayWinner()
+    if (activeGame.gameEnded) {
+        const winnerID = game.winner
+        gameStatus.innerText = winnerID < 2 ? `${players[winnerID]} Wins!` : 'Draw'
+    }
+    else {
+        gameStatus.innerText = `Player ${players[game.activePlayer]} Turn`
     }
 }
 
-//Incomplete
-function displayWinner() {
-
-}
-
-
 const startBtn = document.querySelector('#start-btn')
-const xSymbol = document.querySelector('#x-sym')
-const oSymbol = document.querySelector('#y-sym')
-const winDisplay = document.querySelector('#win-banner')
-const board = document.querySelector('.board-grid')
-
-const grid00 = document.querySelector('#g00')
-const grid01 = document.querySelector('#g01')
-const grid02 = document.querySelector('#g02')
-const grid10 = document.querySelector('#g10')
-const grid11 = document.querySelector('#g11')
-const grid12 = document.querySelector('#g12')
-const grid20 = document.querySelector('#g20')
-const grid21 = document.querySelector('#g21')
-const grid22 = document.querySelector('#g22')
-
+const gameStatus = document.querySelector('#game-status')
+const board = document.querySelectorAll('.board-grid')
+const playerSym = [document.querySelector('#x-sym'), document.querySelector('#y-sym')]
+const players = ['X', 'O']
+const game = new Game()
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
     game.resetBoard()
-    grid00.setAttribute('data-status', 'E')
-    grid01.setAttribute('data-status', 'E')
-    grid02.setAttribute('data-status', 'E')
-    grid10.setAttribute('data-status', 'E')
-    grid11.setAttribute('data-status', 'E')
-    grid12.setAttribute('data-status', 'E')
-    grid20.setAttribute('data-status', 'E')
-    grid21.setAttribute('data-status', 'E')
-    grid22.setAttribute('data-status', 'E')
-    grid00.innerText = ''
-    grid01.innerText = ''
-    grid02.innerText = ''
-    grid10.innerText = ''
-    grid11.innerText = ''
-    grid12.innerText = ''
-    grid20.innerText = ''
-    grid21.innerText = ''
-    grid22.innerText = ''
+    gameStatus.innerText = 'Player X Turn'
+    for (square of board) {
+        square.setAttribute('data-status', 'E')
+        square.innerText = ''
+    }
 })
 
-const game = new Game()
-grid00.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid01.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid01.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid02.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid10.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid11.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid12.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid20.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid21.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
-grid22.addEventListener('click', (event) => {
-    if (!game.endGame) {
-        updateSquare(game, event)
-    }
-})
+for (square of board) {
+    square.addEventListener('click', (event) => {
+        if (!game.gameEnded) {
+            updateSquare(game, event)
+        }
+    })
+}
